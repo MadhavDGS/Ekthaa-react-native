a5a0b3bf5a96e21a9572e1043c74b3d2d838015b
Ekthaa Developer <developer@ekthaa.app>
2026-01-04 21:23:07 +0530

    fix: Correct ImagePicker mediaTypes for expo-image-picker v17
    - Use array format ['images'] instead of deprecated MediaTypeOptions
    - Fixes TypeError: Cannot read property 'Images' of undefined
    - Compatible with expo-image-picker v17.0.10

--- src/screens/Transactions/AddTransactionScreen.tsx
+++ src/screens/Transactions/AddTransactionScreen.tsx
@@ -80,7 +80,7 @@ export default function AddTransactionScreen({ route, navigation }: any) {
       }
 
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaType.Images,
+        mediaTypes: ['images'],
         allowsEditing: true,
         aspect: [4, 3],
         quality: 0.8,
